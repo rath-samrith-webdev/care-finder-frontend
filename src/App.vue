4d0a6b1b3f10e306e4498970a58ba5845cfbc75f
rath-samrith-webdev <rathsamrith.webdev@gmail.com>
2025-02-27 09:51:53 +0700

    chore/set-up-tester: setup test unit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <el-button>{{ $t("test") }}</el-button>
+    <el-button @click="()=>changeLanguage('en')">{{ $t("test") }}</el-button>
+    <el-button @click="()=>changeLanguage('km')">{{ $t("test") }}</el-button>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
 import { setLanguage, getLanguage } from "./common/language";
+import type { ILanguageCode } from "./interfaces/ILanguage";
 export default defineComponent({
   name: "App",
   created() {
     const lang = getLanguage();
     setLanguage(lang);
+  },
+  methods: {
+    changeLanguage(lang: ILanguageCode) {
+      setLanguage(lang);
+    },
   },
 });
 </script>
